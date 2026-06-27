243bca806c1299e304f0b20f7732b4e86b1fc524
akerson <pirodaggar@gmail.com>
2026-06-27 13:36:07 -0400

    fixed scroll and l

--- client/game/js/dispatch.js
+++ client/game/js/dispatch.js
@@ -56,7 +56,7 @@ const handlers = {
 
   look: (msg) => {
     setAreaPane(msg.message);
-    if (state.echoNextLook) { appendHtml(msg.message, 'help'); state.echoNextLook = false; }
+    if (state.echoNextLook) { appendMsg('You look around.', 'system'); state.echoNextLook = false; }
     if (msg.zone) state.currentZone = msg.zone;
     parseZoneInfo(msg.message);
     if (msg.minimap) renderMinimap(msg.minimap);
